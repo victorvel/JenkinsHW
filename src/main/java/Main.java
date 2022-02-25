15d4b61d3994db0e92ad89d090420536c5ddcae0
Victor Velasco <victorvelasco098@gmail.com>
2022-02-25 12:53:39 -0700

    fixed main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,44 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-    String command = args[0];
-    int num1 = Integer.parseInt(args[1]);
-    int num2 = Integer.parseInt(args[2]);
     Calculator calc = new Calculator();
 
-    if (command.equals("add")) {
-      System.out.println(calc.add(num1, num2));
-    }
-    else if (command.equals("subtract")) {
-      System.out.println(calc.subtract(num1, num2));
-    }
-    else if (command.equals("multiply")) {
-      System.out.println(calc.multiply(num1, num2));
-    }
-    else if (command.equals("divide")) {
-      System.out.println(calc.divide(num1, num2));
-    }
-    else if (command.equals("fibonacci")) {
-      System.out.println(calc.fibonacciNumberFinder(num1));
-    }
-    else if (command.equals("binary")) {
-      System.out.println(calc.intToBinaryNumber(num1));
+    boolean continueLoop = true;
+    Scanner userInput;
+    String command;
+    int num1 = 0;
+    int num2 = 0;
+
+    while (continueLoop) {
+      System.out.println("enter command: ");
+      userInput = new Scanner(System.in);
+      String[] arguments = userInput.nextLine().split(" ");
+      command = arguments[0];
+      if (arguments.length > 1) {
+        num1 = Integer.parseInt(arguments[1]);
+      }
+      if (arguments.length > 2) {
+        num2 = Integer.parseInt(arguments[2]);
+      }
+
+      if (command.equals("add")) {
+        System.out.println(calc.add(num1, num2));
+      } else if (command.equals("subtract")) {
+        System.out.println(calc.subtract(num1, num2));
+      } else if (command.equals("multiply")) {
+        System.out.println(calc.multiply(num1, num2));
+      } else if (command.equals("divide")) {
+        System.out.println(calc.divide(num1, num2));
+      } else if (command.equals("fibonacci")) {
+        System.out.println(calc.fibonacciNumberFinder(num1));
+      } else if (command.equals("binary")) {
+        System.out.println(calc.intToBinaryNumber(num1));
+      } else if (command.equals("exit")) {
+        continueLoop = false;
+      }
     }
   }
 }
